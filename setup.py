1fb210a942588c25a8a287984c4f9f4ec1ca5c29
Ed Crewe <cmetc@ilrt.org>
2012-11-04 21:39:09 +0000

    update description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '0.1'
 
 setup(name='django-cookieless',
       version=version,
-      description="Cookie free sessions and decorator",
+      description="Django cookie free sessions optional decorator",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
