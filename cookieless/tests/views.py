129044a6e4c2d081d2b3ac5b25e72710f0ab89e4
Ed Crewe <edmundcrewe@gmail.com>
2012-11-04 13:54:11 +0000

    fix test class view to show session too

--- cookieless/tests/views.py
+++ cookieless/tests/views.py
@@ -3,25 +3,38 @@
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 import datetime
+from django.utils.html import mark_safe
 
 def my_function_view(request):
+    """ Test function view """
     request.session['funcview'] = 'my_function_view'
     html = "<html><body><h1>Function view</h1>"
     html += '<p><a href="/">Class view</a></p><hr />'
-    html += "My session %s.<ul>" % request.session.session_key
-    for key, value in request.session.items():
-        html += "<li>%s = %s </li>" % (key, value)
-    html += '<ul></body></html>'    
+    html += session_data(request)  + '</body></html>'
     return HttpResponse(html)
 
+def session_data(request):
+    """ Utility function to display session """
+    html = "My session %s.<ul>" % request.session.session_key
+    for key, value in request.session.items():
+        html += "<li>%s = %s </li>" % (key, value)
+    html += '<ul>'    
+    return mark_safe(html)
 
 class MyClassView(TemplateView):
+    """ Test class view - with form """
     template_name = "classview.html"
 
     def dispatch(self, *args, **kwargs):
-        args[0].session['classview'] = 'MyClassView'
+        """ Add a session value each time the page is refreshed """
+        request = args[0]
+        request.session['classview'] = 'MyClassView'
         now = datetime.datetime.now()
-        args[0].session[now] = 'more stuff'
+        request.session[now] = 'more stuff'
         return super(MyClassView, self).dispatch(*args, **kwargs)
     
+    def get_context_data(self, **kwargs):
+        context = super(MyClassView, self).get_context_data(**kwargs)
+        context['session_data'] = session_data(self.request)
+        return context
 
