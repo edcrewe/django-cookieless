678b74c591f47a905828f61ab1576919ca0c1ba6
Ed Crewe <cmetc@ilrt.org>
2012-11-07 12:49:02 +0000

    add config

---
+++ cookieless/config.py
@@ -0,0 +1,10 @@
+""" Constants for cookieless """
+LINKS_RE = r'<a(?P<pre_href>[^>]*?)href=["\'](?P<in_href>[^"\']*?)(?P<anchor>#\S+)?["\'](?P<post_href>[^>]*?)>'
+
+DEFAULT_SETTINGS = {'REWRITE' : True,
+                    'USE_GET' : True,
+                    'CLIENT_ID' : True,
+                    'HOSTS' : [],
+                    'NO_COOKIE_PERSIST' : True,
+                    'URL_SPECIFIC' : False
+                    }
